b2672ab79f867ba8f7bbdde2c00765a4e95bed51
Jinhoo Yoon <yoonj23@ashevilleschool.org>
2021-04-27 14:33:24 -0400

    MapKit fiddling, I got a map of America with it centered on Kansas City. The MapKit import already took care of a map of the world. You can also navigate around the map. regionRadius can be changed if it is too big, not yet sure how to add annotation/how to zoom yet.

--- Park Map/ViewController.swift
+++ Park Map/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import MapKit
+class MKMapKit: UIView {
+    
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var MapView: MKMapView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      _ = CLLocation(latitude: 39.099724, longitude: -94.578331)
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
